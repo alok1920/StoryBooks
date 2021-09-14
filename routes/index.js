8f0cd389d6a85004091dd320f4d0fc2c824ea6c9
alok1920 <59192984+alok1920@users.noreply.github.com>
2021-09-14 13:39:10 +0530

    Trying to solve issue of rending files

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
- const express = require('express')
- const router = express.Router()
+const express = require('express')
+const router = express.Router()
 
- // middleware that is specific to this router
+// middleware that is specific to this router
 router.use(function timeLog (req, res, next) {
     console.log('Time: ', Date.now())
     next()
-  })
-
- //@desc    Login/Landing Page
- //@route   GET/
- router.get('/', function (req, res) {
-     res.send('login')
 })
 
-  //@desc    Dashboard
-  //@route   GET/ dashboard 
- router.get('/dashboard', function (req, res) {
+//@desc    Login/Landing Page
+//@route   GET/
+router.get('/', function (req, res, next) {
+    res.render('login')
+})
+
+//@desc    Dashboard
+//@route   GET/ dashboard 
+router.get('/dashboard', (req, res, next ) => {
     res.render('dashboard')
 })
 
- module.exports = router
+module.exports = router
